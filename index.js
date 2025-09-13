c8f4f18b2d83c3a5dccd12c135515da7ca18c271
Jarencio <145330400+Jarencio@users.noreply.github.com>
2025-09-13 11:51:30 +0800

    Revert "index out"
    This reverts commit 99518d0e856d2dd91a004fea059f5eb428750801.

--- index.js
+++
@@ -1,8 +0,0 @@
-let menu = document.querySelector('#menu-icon');
-let nav = document.querySelector('.nav');
-
-menu.onclick = () => {
-    menu.classList.toggle('fa-xmark');
-    nav.classList.toggle('open');
-}
-
